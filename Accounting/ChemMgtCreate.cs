23a4d2c64e80a2bb73a8aa6eadaa93ec45b43152
Jan Ko <eeveed@hotmail.com>
2017-12-12 22:57:39 +0800

    Reduce the quantity of a chemical when someone makes a new request

--- Accounting/ChemMgtCreate.cs
+++ Accounting/ChemMgtCreate.cs
@@ -127,6 +127,15 @@ namespace Accounting
                         MySqlCommand commrequest = new MySqlCommand(queryrequest, conn);
                         commrequest.ExecuteNonQuery();
                         conn.Close();
+
+                        conn.Open();
+
+                        string updateqty = "UPDATE item SET quantity = quantity - " + cqty.Value + " WHERE itemID='" + itemid + "'";
+
+                        MySqlCommand commupdate = new MySqlCommand(updateqty, conn);
+                        commupdate.ExecuteNonQuery();
+                        conn.Close();
+
                         MessageBox.Show("Success", "Request Made", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                         //main.Show();
