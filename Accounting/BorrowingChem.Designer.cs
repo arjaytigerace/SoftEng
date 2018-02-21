69219f7e89e3c7ffd1c09457e9be346d7367139a
jdko <jdko@JF613E-027>
2018-02-21 14:28:29 +0800

    Display the current borrow requests

--- Accounting/BorrowingChem.Designer.cs
+++ Accounting/BorrowingChem.Designer.cs
@@ -34,6 +34,10 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.refresh = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -43,10 +47,10 @@
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(21, 490);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(16, 398);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(269, 46);
+            this.button1.Size = new System.Drawing.Size(202, 37);
             this.button1.TabIndex = 51;
             this.button1.Text = "Borrow New Item";
             this.button1.UseVisualStyleBackColor = false;
@@ -57,10 +61,9 @@
             this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
             this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox4.Location = new System.Drawing.Point(-1, 3);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox4.Location = new System.Drawing.Point(-1, 2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(91, 46);
+            this.pictureBox4.Size = new System.Drawing.Size(68, 37);
             this.pictureBox4.TabIndex = 60;
             this.pictureBox4.TabStop = false;
             this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
@@ -70,10 +73,10 @@
             this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(497, 490);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Location = new System.Drawing.Point(373, 398);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(127, 46);
+            this.button4.Size = new System.Drawing.Size(123, 37);
             this.button4.TabIndex = 53;
             this.button4.Text = "Return";
             this.button4.UseVisualStyleBackColor = false;
@@ -84,10 +87,10 @@
             this.button3.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(310, 490);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(232, 398);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(165, 46);
+            this.button3.Size = new System.Drawing.Size(124, 37);
             this.button3.TabIndex = 78;
             this.button3.Text = "Update";
             this.button3.UseVisualStyleBackColor = false;
@@ -97,30 +100,79 @@
             // 
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(21, 71);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 58);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(1110, 381);
+            this.dataGridView1.Size = new System.Drawing.Size(832, 310);
             this.dataGridView1.TabIndex = 77;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(527, 409);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 79;
+            this.label1.Text = "Number Borrowed:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(646, 409);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 80;
+            this.label2.Text = "label2";
+            // 
+            // refresh
+            // 
+            this.refresh.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.refresh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.refresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refresh.Location = new System.Drawing.Point(373, 439);
+            this.refresh.Margin = new System.Windows.Forms.Padding(2);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(123, 37);
+            this.refresh.TabIndex = 81;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = false;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(123, 439);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 37);
+            this.button2.TabIndex = 82;
+            this.button2.Text = "Returned Items";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
             // BorrowingChem
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.ClientSize = new System.Drawing.Size(1155, 617);
+            this.ClientSize = new System.Drawing.Size(866, 501);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "BorrowingChem";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BorrowingChem";
@@ -129,6 +181,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -138,5 +191,9 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Accounting/BorrowingChem.cs
+++ Accounting/BorrowingChem.cs
@@ -67,7 +67,59 @@ namespace Accounting
         private void BorrowingChem_Load(object sender, EventArgs e)
         {
 
-            
+            Loadall();
+
+
+        }
+
+        public void Loadall()
+        {
+
+            string query = "SELECT borrowRequestId,b.itemName,CONCAT(a.qty,' ',a.measureType) AS qty,CONCAT(studentFName,' ',studentLName) AS student,c.studentId,c.yearCourse," +
+                "a.borrowedDate,a.returnDate,CONCAT(e.firstname,' ',e.lastname) AS releasedBy, a.requestStatus" +
+                " FROM chem_lab.borrowing a INNER JOIN administrativeassociate e ON a.releasedBy = e.admin_ID, " +         
+                "chem_lab.item b, chem_lab.student c WHERE b.itemID = a.itemID" +
+                " AND c.studentID = a.studentId";
+
+            conn.Open();
+            MySqlCommand comm = new MySqlCommand(query, conn);
+            MySqlDataAdapter adp = new MySqlDataAdapter(comm);
+            conn.Close();
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.DataSource = dt;
+
+            dataGridView1.Columns["borrowRequestId"].Visible = false;
+            dataGridView1.Columns["studentId"].Visible = false;
+    
+            dataGridView1.Columns["itemName"].HeaderText = "Item Name";
+            dataGridView1.Columns["itemName"].Width = 150;
+            dataGridView1.Columns["qty"].HeaderText = "QTY Requested";
+            dataGridView1.Columns["qty"].Width = 90;
+            dataGridView1.Columns["student"].HeaderText = "Borrowed By";
+            dataGridView1.Columns["yearCourse"].HeaderText = "Course";
+            dataGridView1.Columns["borrowedDate"].HeaderText = "Date Borrowed";
+      
+            dataGridView1.Columns["returnDate"].HeaderText = "Expected Return Date";
+            dataGridView1.Columns["releasedBy"].HeaderText = "Released By";
+            dataGridView1.Columns["requestStatus"].HeaderText = "Status";
+
+            dataGridView1.ClearSelection();
+
+           
+
+            string query1 = "SELECT COUNT(*) from borrowing";
+
+            conn.Open();
+            MySqlCommand comm1 = new MySqlCommand(query1, conn);
+            MySqlDataAdapter adp1 = new MySqlDataAdapter(comm1);
+            conn.Close();
+            DataTable dt1 = new DataTable();
+            adp1.Fill(dt1);
+            label2.Text = dt1.Rows[0]["COUNT(*)"].ToString();
+
 
 
         }
@@ -87,6 +139,11 @@ namespace Accounting
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            Loadall();
         }
     }
 }
