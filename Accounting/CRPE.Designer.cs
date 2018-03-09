bd74f27547ad0545b957948e9b0793890d52d9db
arjaytigerace <raeniel2098@gmail.com>
2018-03-09 13:04:31 +0800

    f

--- Accounting/CRPE.Designer.cs
+++ Accounting/CRPE.Designer.cs
@@ -39,14 +39,14 @@
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.Size = new System.Drawing.Size(312, 482);
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1132, 645);
             this.crystalReportViewer1.TabIndex = 0;
             // 
             // CRPE
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(312, 482);
+            this.ClientSize = new System.Drawing.Size(1132, 645);
             this.Controls.Add(this.crystalReportViewer1);
             this.Name = "CRPE";
             this.Text = "CRPE";
